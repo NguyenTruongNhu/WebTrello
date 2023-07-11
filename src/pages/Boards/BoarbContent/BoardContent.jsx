4bd98b203e8d4f7d773011430b9950f5fb2af25e
NguyenTruongNhu <nhu81632@gmail.com>
2023-07-11 11:00:15 +0700

    drag_drop_column

--- src/pages/Boards/BoarbContent/BoardContent.jsx
+++ src/pages/Boards/BoarbContent/BoardContent.jsx
@@ -1,20 +1,62 @@
 import Box from '@mui/material/Box'
 import ListColumns from './ListColumns/ListColumns'
 import { mapOrder } from '~/utils/sorts'
+import {
+  DndContext,
+  useSensor,
+  useSensors,
+  MouseSensor,
+  TouchSensor
+} from '@dnd-kit/core'
+import { useState, useEffect } from 'react'
+import { arrayMove } from '@dnd-kit/sortable'
 function BoardContent({ board }) {
-  const orderedColumns = mapOrder(board?.columns, board?.columnOrderIds, '_id')
+  // yêu cầu chuột di chuyển 10px thì mới kích hoạt event, fix trường hợp bị gọi event
+
+  const mouseSensor = useSensor(MouseSensor, {
+    activationConstraint: { distance: 10 }
+  })
+  const touchSensor = useSensor(TouchSensor, {
+    activationConstraint: { delay: 250, tolerance: 500 }
+  })
+  // const sensors = useSensors(pointerSensor)
+  const sensors = useSensors(mouseSensor, touchSensor)
+  const [orderedColumns, setOrderedColumns] = useState([])
+
+  useEffect(() => {
+    setOrderedColumns(mapOrder(board?.columns, board?.columnOrderIds, '_id'))
+  }, [board])
+  const handleDragEnd = (event) => {
+    const { active, over } = event
+    // kiểm tra nếu không tồn tại over
+    if (!over) return
+    if (active.id !== over.id) {
+      // lấy vị trí củ
+      const oldIndex = orderedColumns.findIndex((c) => c._id === active.id)
+      // Lấy vị trí mới
+      const newIndex = orderedColumns.findIndex((c) => c._id === over.id)
+
+      const dndOrderedColumns = arrayMove(orderedColumns, oldIndex, newIndex)
+      // const dndOrderedColumnsIds = dndOrderedColumns.map((c) => c._id)
+      // console.log(dndOrderedColumns)
+      // console.log(dndOrderedColumnsIds)
+      setOrderedColumns(dndOrderedColumns)
+    }
+  }
   return (
-    <Box
-      sx={{
-        bgcolor: (theme) =>
-          theme.palette.mode == 'dark' ? '#34495e' : '#1976d2',
-        width: '100%',
-        height: (theme) => theme.trello.boardContentHeight,
-        p: '10px 0'
-      }}
-    >
-      <ListColumns columns={orderedColumns} />
-    </Box>
+    <DndContext onDragEnd={handleDragEnd} sensors={sensors}>
+      <Box
+        sx={{
+          bgcolor: (theme) =>
+            theme.palette.mode == 'dark' ? '#34495e' : '#1976d2',
+          width: '100%',
+          height: (theme) => theme.trello.boardContentHeight,
+          p: '10px 0'
+        }}
+      >
+        <ListColumns columns={orderedColumns} />
+      </Box>
+    </DndContext>
   )
 }
 
